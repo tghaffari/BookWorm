e10c9fdd58ded22876ee74404eb631330fc017f7
Tarra Ghaffari <tarrag2525@gmail.com>
2022-09-24 00:12:02 +0000

    Added placeholders for the homepage in case no books have been saved for issue-5-user-can-view-home-page.

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -23,7 +23,6 @@ export default class Home extends React.Component {
             unreadBooks.unshift(book);
           }
         });
-
         this.setState({ readBooks, unreadBooks });
       })
       .catch(err => console.error(err));
@@ -38,32 +37,50 @@ export default class Home extends React.Component {
       return null;
     }
 
-    const readBookCovers = this.state.readBooks.map(book => {
-      return (
-        <li className='column-flex cover-li' key={book.googlId}>
-          <img className='home-cover-img' src={book.coverImgURL} />
-        </li>
-      );
-    });
+    let readBookCovers;
+    let unreadBookCovers;
 
-    const unreadBookCovers = this.state.unreadBooks.map(book => {
-      console.log(book);
-      return (
-        <li className='column-flex cover-li' key={book.googlId}>
-          <img className='home-cover-img' src={book.coverImgURL} />
+    if (this.state.readBooks.length === 0) {
+      readBookCovers = (
+        <li className='column-full text-align-center placeholder-column-size' key='1'>
+          <img className='placeholder-books' src='/images/placeholder_books.png'></img>
+          <p className='home-placeholder-text'>Click Search to start adding books to your bookshelf!</p>
         </li>
       );
-    });
+    } else {
+      readBookCovers = this.state.readBooks.map(book => {
+        return (
+          <li className='column-flex cover-li' key={book.googlId}>
+            <img className='home-cover-img' src={book.coverImgURL} />
+          </li>
+        );
+      });
+    }
+
+    if (this.state.unreadBooks.length === 0) {
+      unreadBookCovers = (
+        <li className='column-full text-align-center placeholder-column-size' key='1'>
+          <img className='placeholder-books' src='/images/placeholder_books.png'></img>
+          <p className='home-placeholder-text'>Click Search to start adding books to your bookshelf!</p>
+        </li>
+      );
+    } else {
+      unreadBookCovers = this.state.unreadBooks.map(book => {
+        return (
+          <li className='column-flex cover-li' key={book.googlId}>
+            <img className='home-cover-img' src={book.coverImgURL} />
+          </li>
+        );
+      });
+    }
 
     return (
       <>
-
-      <div className='row text-align-center'>
-        <div className='column-full'>
-            <a href='#library'><h1 className='home-read-shelf-title'>Read Bookshelf</h1></a>
+        <div className='row text-align-center'>
+          <div className='column-full'>
+              <a href='#library'><h1 className='home-read-shelf-title'>Read Bookshelf</h1></a>
+          </div>
         </div>
-      </div>
-
         <div className='row'>
           <div className='column-full'>
             <ul className='row read-shelf-list justify-content-evenly'>
@@ -76,30 +93,24 @@ export default class Home extends React.Component {
             </div>
           </div>
         </div>
-
-      <div className='row text-align-center'>
-        <div className='column-full'>
-          <a href='#library'><h1 className='home-to-read-shelf-title'>To-Read Bookshelf</h1></a>
+        <div className='row text-align-center'>
+          <div className='column-full'>
+            <a href='#library'><h1 className='home-to-read-shelf-title'>To-Read Bookshelf</h1></a>
+          </div>
         </div>
-      </div>
-
-      <div className='row'>
-        <div className='column-full'>
-          <ul className='row to-read-shelf-list justify-content-evenly'>
-            {unreadBookCovers}
-          </ul>
-          <div className='row text-align-center'>
-            <div className='column-full'>
-              <img src='/images/shelf.png' className='to-read-shelf'></img>
+        <div className='row'>
+          <div className='column-full'>
+            <ul className='row to-read-shelf-list justify-content-evenly'>
+              {unreadBookCovers}
+            </ul>
+            <div className='row text-align-center'>
+              <div className='column-full'>
+                <img src='/images/shelf.png' className='to-read-shelf'></img>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
       </>
-
     );
   }
 }
-
-// Start adding books to your bookshelf!
