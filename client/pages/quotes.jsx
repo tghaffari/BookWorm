1cdb8cf29893b8a9bb897f4d717aa5d1bdfe349c
Tarra Ghaffari <tarrag2525@gmail.com>
2022-11-10 23:03:01 +0000

    Added quotes to render quotes without any styling.

--- client/pages/quotes.jsx
+++ client/pages/quotes.jsx
@@ -22,10 +22,7 @@ export default class Quotes extends React.Component {
 
     fetch('/api/getQuotes', init)
       .then(res => res.json())
-      .then(quotes => {
-        // console.log('quotes:', quotes);
-        this.setState({ quotes });
-      })
+      .then(quotes => this.setState({ quotes }))
       .catch(err => {
         console.error(err);
         if (this.context.user) window.alert('Sorry, we are unable to retrieve your quotes at this time. Please check your internet connection and try again later.');
@@ -35,7 +32,36 @@ export default class Quotes extends React.Component {
   render() {
     if (!this.context.user) return <Redirect to="sign-in" />;
 
-    if (this.state.myBooks === null) return null;
+    if (this.state.quotes === null) return null;
+
+    if (this.state.quotes.length === 0) {
+      // console.log('hi');
+    }
+
+    const quotes = this.state.quotes.map(quotes => {
+      // console.log(quotes);
+      const { quote, author, title, pageNumber } = quotes;
+      // add a condition in case author doesn't exist.
+      return (
+        <li key={pageNumber}>
+          <p className='quote'> &quot;{quote}&quot;</p>
+          <p className='quote-details'>- ${author},
+            <span className='quote-title-page-number'> {title}, {pageNumber}
+            </span>
+          </p>
+        </li>
+      );
+    });
+
+    return (
+      <>
+        <h1 className='quotes-header'>Favorite Quotes</h1>
+        <ul className='quotes-list'>
+          {quotes}
+        </ul>
+      </>
+    );
+
   }
 
 }
